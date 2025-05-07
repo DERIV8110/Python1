b46d481b3abd544efbc1a3accd584717fdcf5282
Abhilash Ray <abhilashrai9896@gmail.com>
2025-05-08 00:59:55 +0530

    Add files via upload

---
+++ list with both indexes.py
@@ -0,0 +1,6 @@
+a=['q','w','e','r','t','y']
+i=0
+length=len(a)
+for i in range (length):
+    print( "at indexes:" ,[i],"and:", [-length+i],"element:", a[i] )
+    i=+1
---
+++ print list elements in diff lines.py
@@ -0,0 +1,9 @@
+a=['q','w','e','r','t','y']
+i=0
+length=len(a)
+j=6
+x=0
+for i in range (length):
+    print( "at indexes:" ,[i],"and:", [x-length],"element:", a[i] )
+    i=+1
+    x=+1
