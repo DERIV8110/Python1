7e89c21f52963a97b95162572d2476c6fca8a602
Abhilash Ray <abhilashrai9896@gmail.com>
2025-07-17 09:03:52 +0530

    Add files via upload

---
+++ pretty printing with json.py
@@ -0,0 +1,7 @@
+import json
+d1={"Suniti":{1:40,2:70,3:70}}
+d2={"ryna":{1:40,2:50,3:60}}
+d3={"zeba":{1:70,2:80,3:90}}
+print(json.dumps(d1,indent=4))
+print(json.dumps(d2,indent=4))
+print(json.dumps(d3,indent=4))
