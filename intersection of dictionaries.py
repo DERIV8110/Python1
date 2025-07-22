2841bfbdc5477acf7984307138da9673c8736209
Abhilash Ray <abhilashrai9896@gmail.com>
2025-07-22 13:48:56 +0530

    Add files via upload

---
+++ intersection of dictionaries.py
@@ -0,0 +1,8 @@
+
+d1={'a':1,'b':2,'c':3}
+d2={'d':4,'c':3,'e':5}
+common_keys = d1.keys() & d2.keys()     #& is called intersection
+print(common_keys)
+
+#lot's of wrong info everywhere about this problem
+#thi solution is perfecr
