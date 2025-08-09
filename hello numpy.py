644c7b11a4e6021dba38d2911630b32ede4aec9c
Abhilash Ray <abhilashrai9896@gmail.com>
2025-08-10 05:14:59 +0530

    Add files via upload

---
+++ hello numpy.py
@@ -0,0 +1,4 @@
+import numpy as np
+list=[1,2,3,4]
+a1=np.array(list)
+print(a1[3])
---
+++ scalar op on 4x4 matrix.py
@@ -0,0 +1,8 @@
+import numpy as np
+a1=np.array(([1,2,3,4],
+             [5,6,7,8],
+             [9,10,11,12],
+             [13,14,15,16]))  #it is compulsory to use double brackets,
+                              #to pass comma separated argu at once
+a2=np.add(a1,2)
+print(a2)
