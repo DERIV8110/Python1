e2675831e212487b0f4795c23687a413ab3e6f39
Abhilash Ray <abhilashrai9896@gmail.com>
2025-08-11 03:50:05 +0530

    Add files via upload

---
+++ finding an irrational between two irrationals using linspace.py
@@ -0,0 +1,6 @@
+import numpy as np
+import math as mt
+a1=np.linspace(mt.sqrt(2),mt.sqrt(3),8) #first and last values are always
+                                        #start and stop, so for 1 item in b/w
+                                        #2 write 3 items to generate
+print(a1)
