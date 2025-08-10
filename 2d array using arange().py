5ffa86532d8dff8c29a858103de6ce8e5de47970
Abhilash Ray <abhilashrai9896@gmail.com>
2025-08-11 04:22:08 +0530

    Add files via upload

---
+++ 2d array using arange().py
@@ -0,0 +1,4 @@
+import numpy as np
+a1=np.arange(15)
+a2=a1.reshape(5,3)  # remember: no. of elements=rowsxcolumns
+print(a2)
---
+++ creating 2d array with array().py
@@ -0,0 +1,4 @@
+import numpy as np
+list1=[[1,3,5],[2,4,6]]
+a1=np.array(list1)
+print(a1)
