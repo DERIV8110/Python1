973a85992c33fc31585567f29382172b89d1f6ab
Abhilash Ray <abhilashrai9896@gmail.com>
2025-08-14 02:28:16 +0530

    Add files via upload

---
+++ empty arrays using empty().py
@@ -0,0 +1,5 @@
+import numpy as np
+ar1=np.empty([3,2],dtype=np.int64)
+#2D empty array of 3 rows and 2 columns has been created
+#the empty array isn't empty, it comtains garbage/tempo values
+print(ar1)
